984356ac11f4c3649f7117227e443c06b43140a2
Michal Kizling <mkizling@gmail.com>
2017-06-06 12:25:05 +0200

    basic project files

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// scripts.js
+
+var name = prompt('Enter your name');
+/* Long enough comment 
+	I just want it to look full of smart things
+*/
+alert('Hello, ' + name);
+// Other comment
+console.log('Hello, ' + name
+// And one more
